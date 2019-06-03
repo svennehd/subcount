0967e0d8a15e1c8229b9c93376ac386869daf8cd
Melker Heino <melker@filur.org>
2019-06-03 12:27:28 +0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -21,19 +21,7 @@ function App() {
   useInterval(() => {
     fetchData();
   }, 5000);
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const result = await axios(
-  //       'https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCWbMGyXXdOqlUf_6bWyhwqg&key=AIzaSyBeNbq8rtdqxFgT3P8SLAY6Vd2jq0ewciw',
-  //     );
-
-  //     setData(result.data.items[0].statistics.subscriberCount);
-  //   };
-
-  //   fetchData();
-  // }, []);
-
+  
   return (
     <div className="App">
       <header className="App-header">
